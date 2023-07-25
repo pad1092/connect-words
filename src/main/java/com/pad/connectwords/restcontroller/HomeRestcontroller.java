d99cfcaf069a4876d4cdfb3c229238d14f1a335e
unknown <phamduc10920@gmail.com>
2023-07-25 17:18:53 +0700

    fixed cross origin

--- src/main/java/com/pad/connectwords/restcontroller/HomeRestcontroller.java
+++ src/main/java/com/pad/connectwords/restcontroller/HomeRestcontroller.java
@@ -3,15 +3,13 @@ package com.pad.connectwords.restcontroller;
 import com.pad.connectwords.Entity.Player;
 import com.pad.connectwords.service.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(allowCredentials = "*")
 public class HomeRestcontroller {
     @Autowired
     IndexService indexService;
--- src/main/java/com/pad/connectwords/restcontroller/PlayerRestcontroller.java
+++ src/main/java/com/pad/connectwords/restcontroller/PlayerRestcontroller.java
@@ -1,6 +1,7 @@
 package com.pad.connectwords.restcontroller;
 
 import com.pad.connectwords.Entity.Player;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(allowCredentials = "*")
 public class PlayerRestcontroller {
     @GetMapping("/player")
     public Player getPlayer(HttpSession session){
